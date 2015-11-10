a4ec8f22d69c638d70cb7af78c62e897b8db5f2e
c.s.tilstra <cstilstra@gmail.com>
2015-11-10 12:23:03 -0500

    line_count_driver.run_directory_recursively(directory_name) returns the correct counts of comment and code lines

--- line_count_driver.rb
+++ line_count_driver.rb
@@ -28,12 +28,25 @@ class LineCountDriver
   end # End run_one_file()
 
   def run_directory_recursively(directory_name)
-    return "directory run recursively: " + directory_name
-    # TODO: Check if is a file or a directory
-    # TODO: If file, run_one_file(), else
-    # TODO: Get a list of all files and subdirectories in the directory
-    # TODO: Loop through list of files and subdirectories, add_response_to_counts(recursive call) for each
-    # TODO: Return counts
+    #return "directory run recursively: " + directory_name
+    # Check if is a file or a directory
+    if File.file?(directory_name)
+      # If file, run file
+      return run_one_file(directory_name)
+    elsif File.directory?(directory_name)
+      # Get a list of all files and subdirectories in the directory
+      file_names = Dir.entries(directory_name)
+      # TODO: Loop through list of files and subdirectories, add_response_to_counts(recursive call) for each
+      file_names.each do |file|
+        if file != ".." && file != "."
+          add_response_to_counts(run_directory_recursively(directory_name + "/" + file))
+        end # End if file != ".." && file != "."
+      end # End file_names.each do
+      # TODO: Return counts
+      return [@comment_count, @code_count]
+    else
+      return [0,0]
+    end # End if File.file?()
   end # End run_directory_recursively()
 
   def add_response_to_counts(response)
