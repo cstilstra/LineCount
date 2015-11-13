b4a810238134b29a1f9ba2f2aecd88e8132c258f
c.s.tilstra <cstilstra@gmail.com>
2015-11-13 15:28:48 -0500

    All tests pass, program works when line_count_driver is used as entry point

--- spec/line_count_driver_spec.rb
+++ spec/line_count_driver_spec.rb
@@ -7,8 +7,7 @@ describe LineCountDriver do
 
   # Tests for initialize()
   it "initialize() creates the proper objects (FileReader)" do
-    temp_driver = LineCountDriver.new
-    reader = temp_driver.reader
+    reader = driver.reader
     expect(reader).to be_truthy
   end
 
@@ -19,8 +18,8 @@ describe LineCountDriver do
     test_file.puts("# Four comments")
     test_file.puts("# One line of code")
     test_file.puts("one_line_of_code = 1")
-    test_file.puts("begin=")
-    test_file.puts("end=")
+    test_file.puts("=begin")
+    test_file.puts("=end")
     test_file.close
     result = driver.run_one_file("test.rb")
     expect(result).to eq([4,1])
@@ -31,20 +30,32 @@ describe LineCountDriver do
 
   # Tests for run_directory_recursively()
   it "run_directory_recursively(directory_name) returns the correct counts of comment and code lines" do
-    response = driver.run_directory_recursively("./languages")
+    driver.comment_count = 0
+    driver.code_count = 0
+    response = driver.run_directory_recursively("languages")
     expect(response).to eq([0, 38])
   end
 
   # Tests for add_response_to_counts()
   it "add_response_to_counts(response) correctly increments both counters" do
-    # Initialize a new driver because we want to guarantee counts will start at zero
-    temp_driver = LineCountDriver.new
-    temp_driver.add_response_to_counts([4,1])
-    expect(temp_driver.comment_count).to eq(4)
-    expect(temp_driver.code_count).to eq(1)
-    temp_driver.add_response_to_counts([2,17])
-    expect(temp_driver.comment_count).to eq(6)
-    expect(temp_driver.code_count).to eq(18)
+    driver.comment_count = 0
+    driver.code_count = 0
+    driver.add_response_to_counts([4,1])
+    expect(driver.comment_count).to eq(4)
+    expect(driver.code_count).to eq(1)
+    driver.add_response_to_counts([2,17])
+    expect(driver.comment_count).to eq(6)
+    expect(driver.code_count).to eq(18)
   end
+
+  # Tests for handle_command_line_arguments()
+  it "handle_command_line_arguments() returns the correct values for the given arguments" do
+    driver.comment_count = 0
+    driver.code_count = 0
+    arguments = ["languages"]
+    response = driver.handle_command_line_arguments(arguments)
+    expect(response).to eq(["0 comment lines", "38 code lines"])
+  end
+
 
 end
--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 # Four comments
 # One line of code
 one_line_of_code = 1
-begin=
-end=
+=begin
+=end
